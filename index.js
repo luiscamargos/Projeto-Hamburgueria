fda5f92fa4dadbdc11c406aa4f4d17b46c97196a
Luis Gabriel <166856335+luiscamargos@users.noreply.github.com>
2024-06-24 15:38:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+
+window.alert('SEJAM BEM VINDOS')
+
+
+window.revelar = ScrollReveal({reset:true})
+
+
+revelar.reveal('.efeito-txt',
+    {
+        duration:3000,
+        distance:'90px'
+    }
+)
+revelar.reveal('.efeito-img',
+    {
+        duration:3000,
+        distance:'90px'
+    }
+)
